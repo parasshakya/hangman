caf3e0f1b1d6af9d9470228316b5cfba66e2153b
Paras Shakya <parasshakya8085@gmail.com>
2023-04-07 14:15:22 +0545

    changes

---
+++ lib/GameScreen.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+
+import 'constants.dart';
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  State<GameScreen> createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  String characters = 'abcdefghijklmnopqrstuvwxyz'.toUpperCase();
+  String words = 'paras'.toUpperCase();
+  List<String> selectedChar = [];
+  int tries = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                flex: 2,
+                child: Column(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: Container(
+                          padding: const EdgeInsets.all(30),
+                          child: Stack(
+                            children: [
+                              buildHangMan(tries >= 0, HangManParts.pole),
+                              buildHangMan(tries >= 1, HangManParts.head),
+                              buildHangMan(tries >= 2, HangManParts.body),
+                              buildHangMan(tries >= 3, HangManParts.leftHand),
+                              buildHangMan(tries >= 4, HangManParts.rightHand),
+                              buildHangMan(tries >= 5, HangManParts.leftLeg),
+                              buildHangMan(tries >= 6, HangManParts.rightLeg),
+                            ],
+                          )),
+                    ),
+                    Expanded(
+                        child: Container(
+                            color: Colors.teal,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: words
+                                  .split('')
+                                  .map((e) => hiddenLetter(e, !selectedChar.contains(e)))
+                                  .toList(),
+                            )))
+                  ],
+                )),
+            Expanded(
+                child: Container(
+              color: Colors.deepPurple,
+              child: GridView.count(
+                padding: const EdgeInsets.all(12),
+                crossAxisCount: 7,
+                mainAxisSpacing: 8,
+                crossAxisSpacing: 6,
+                children: characters
+                    .split('')
+                    .map((e) => ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            elevation: 0, backgroundColor: Colors.black),
+                        onPressed: selectedChar.contains(e)
+                            ? null
+                            : () {
+                                setState(() {
+                                  selectedChar.add(e);
+                                  if (!words.contains(e)) {
+                                    tries++;
+                                  }
+                                });
+                              },
+                        child: Text(
+                          e,
+                          style: const TextStyle(fontSize: 24),
+                        )))
+                    .toList(),
+              ),
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+Widget hiddenLetter(String char, bool visible) {
+  return Container(
+    width: 60,
+    height: 60,
+    decoration: BoxDecoration(
+      color: Colors.white,
+      borderRadius: BorderRadius.circular(12)
+    ),
+
+    alignment: Alignment.center,
+    child: Visibility(
+        visible: !visible,
+        child: Text(
+          char,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        )),
+  );
+}
+
+Widget buildHangMan(bool visible, String path) {
+  return Visibility(
+    visible: visible,
+    child: Image.asset(path),
+  );
+}
---
+++ lib/constants.dart
@@ -0,0 +1,10 @@
+class HangManParts{
+    static const pole = 'assets/images/0.png';
+    static const head = 'assets/images/1.png';
+    static const body = 'assets/images/2.png';
+    static const leftHand = 'assets/images/3.png';
+    static const rightHand = 'assets/images/4.png';
+    static const leftLeg = 'assets/images/5.png';
+    static const rightLeg = 'assets/images/6.png';
+}
+
